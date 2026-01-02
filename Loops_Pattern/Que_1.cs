c08b0748be87abbc73c673b241d1a087b706daa2
sushilkumar9096 <107684018+sushilkumar9096@users.noreply.github.com>
2026-01-03 01:01:23 +0530

    pattern

---
+++ Loops_Pattern/Que_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Loops_Pattern
+{
+    internal class Que_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*\r\n* *\r\n* * *\r\n* * * *\r\n* * * * *\r\n");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
